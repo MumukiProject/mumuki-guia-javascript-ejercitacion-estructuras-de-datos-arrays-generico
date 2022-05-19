b38ba98edce3237b888eef6ece04529ea35d1781
gustavo@mumuki.org <gustavo@mumuki.org>
2022-05-19 15:57:41 +0000

    Mumuki Export on 2022-05-19 15:57:41 +0000

--- 00015_Obtener resultado/description.md
+++ 00015_Obtener resultado/description.md
@@ -1,4 +1,4 @@
-> Definí una función `obtenerResultado` que tome por parámetros dos strings `jugadoraA` y `jugadoraB` con los nombres de cada jugadora respectivamente, y dos arrays de numeros `puntajesA` y `puntajesB` de la misma longitud. La función debe devolver un string con el nombre de la ganadora o `Empate` en caso de que no haya ninguna. Para eso, debe comparar las mismas posiciones de cada array de puntajes, y sumar puntos a la jugadora correspondiente dependiendo de quien tenga el puntaje más alto. Por ejemplo:
+> Definí una función `obtenerResultado` que tome por parámetros dos strings `jugadorA` y `jugadorB` con los nombres de cada jugador respectivamente, y dos arrays de numeros `puntajesA` y `puntajesB` de la misma longitud. La función debe devolver un string con el nombre del ganador o `Empate` en caso de que no haya ninguno. Para eso, debe comparar las mismas posiciones de cada array de puntajes, y sumar puntos al jugador correspondiente dependiendo de quien tenga el puntaje más alto. Por ejemplo:
 >
 ```javascript
 const puntajesA = [3, 5, 2]
@@ -8,14 +8,14 @@ const puntajesB = [4, 6, 2]
 // puntajesA[1] vs. puntajesB[1] -> Gana B
 // puntajesA[2] vs. puntajesB[2] -> Empate
 >
-// Resultado final: Gana Jugadora B
+// Resultado final: Gana Jugador B
 ```
 > 
 ```javascript
-ム obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3])
-Ada
-ム obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9])
+ム obtenerResultado('Ale', 'Gaby', [4, 4, 4], [1, 2, 3])
+Ale
+ム obtenerResultado('Ale', 'Gaby', [3, 5, 5, 7], [4, 1, 2, 9])
 Empate
-ム obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3])
-Grace
+ム obtenerResultado('Ale', 'Gaby', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3])
+Gaby
 ```
--- 00015_Obtener resultado/test..js
+++ 00015_Obtener resultado/test..js
@@ -1,14 +1,14 @@
-it("Si hacemos obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3]) debería retornar Ada", function() {
-let resultado = obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3])
-assert.deepEqual(resultado, 'Ada',`obtenerResultado('Ada', 'Grace', [4, 4, 4], [1, 2, 3]) debería retornar 'Ada' pero fue ${resultado}`);
+it("Si hacemos obtenerResultado('Ale', 'Gaby', [4, 4, 4], [1, 2, 3]) debería retornar Ale", function() {
+let resultado = obtenerResultado('Ale', 'Gaby', [4, 4, 4], [1, 2, 3])
+assert.deepEqual(resultado, 'Ale',`obtenerResultado('Ale', 'Gaby', [4, 4, 4], [1, 2, 3]) debería retornar 'Ale' pero fue ${resultado}`);
 })
 
-it("Si hacemos obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9]) debería retornar Empate", function() {
-let resultado = obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9])
-assert.deepEqual(resultado, 'Empate',`obtenerResultado('Ada', 'Grace', [3, 5, 5, 7], [4, 1, 2, 9]) debería retornar 'Empate' pero fue ${resultado}`);
+it("Si hacemos obtenerResultado('Ale', 'Gaby', [3, 5, 5, 7], [4, 1, 2, 9]) debería retornar Empate", function() {
+let resultado = obtenerResultado('Ale', 'Gaby', [3, 5, 5, 7], [4, 1, 2, 9])
+assert.deepEqual(resultado, 'Empate',`obtenerResultado('Ale', 'Gaby', [3, 5, 5, 7], [4, 1, 2, 9]) debería retornar 'Empate' pero fue ${resultado}`);
 })
 
-it("Si hacemos obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3]) debería retornar Grace", function() {
-let resultado = obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3])
-assert.deepEqual(resultado, 'Grace',`obtenerResultado('Ada', 'Grace', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3]) debería retornar 'Grace' pero fue ${resultado}`);
+it("Si hacemos obtenerResultado('Ale', 'Gaby', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3]) debería retornar Gaby", function() {
+let resultado = obtenerResultado('Ale', 'Gaby', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3])
+assert.deepEqual(resultado, 'Gaby',`obtenerResultado('Ale', 'Gaby', [5, 6, 3, 1, 8], [8, 2, 4, 2, 3]) debería retornar 'Gaby' pero fue ${resultado}`);
 })
