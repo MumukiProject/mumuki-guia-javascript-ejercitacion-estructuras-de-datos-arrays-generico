9f32fc5589f773bb1f4ac90ead259ba2a7a6a7b9
mayra@mumuki.org <mayra@mumuki.org>
2022-05-20 20:14:57 +0000

    Mumuki Export on 2022-05-20 20:14:57 +0000

--- 00004_Invertir caso/test..js
+++ 00004_Invertir caso/test..js
@@ -1,6 +1,6 @@
-it("Si hacemos invertirCaso('Ada Lovelace')  debería retornar 'aDA lOVELACE'", function() {
-let resultado = invertirCaso('Ada Lovelace') 
-assert.equal(resultado, 'aDA lOVELACE',`invertirCaso('Ada Lovelace')  debería retornar 'aDA lOVELACE' pero fue ${resultado}`);
+it("Si hacemos invertirCaso('jUAnA aZurDuY')  debería retornar 'JuaNa AzURdUy'", function() {
+let resultado = invertirCaso('jUAnA aZurDuY') 
+assert.equal(resultado, 'JuaNa AzURdUy',`invertirCaso('jUAnA aZurDuY')  debería retornar 'JuaNa AzURdUy' pero fue ${resultado}`);
 })
 
 it("Si hacemos invertirCaso('feliz cumple')  debería retornar 'FELIZ CUMPLE'", function() {
