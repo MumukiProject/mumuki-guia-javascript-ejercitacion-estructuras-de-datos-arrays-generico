e12ba586773d88e2ef9e8cfd4b6cc9cc3e20fee5
gustavo@mumuki.org <gustavo@mumuki.org>
2022-06-06 15:53:52 +0000

    Mumuki Export on 2022-06-06 15:53:52 +0000

--- 00003_Contiene/description.md
+++
@@ -1,8 +0,0 @@
-> Definí una función `contiene` que reciba como argumentos un número `numero` y un array de números `numeros` y devuelva si el número se encuentra en dicho array.
->
-```javascript
-ム contiene(54, [5, 7, 99, 3, 4, 54, 2, 12])
-true
-ム contiene(103, [5, 7, 99, 3, 4, 54, 2, 12])
-false
-```
--- 00003_Contiene/test..js
+++
@@ -1,9 +0,0 @@
-it("Si hacemos contiene(54, [5, 7, 99, 3, 4, 54, 2, 12]) debería retornar true", function() {
-let resultado = contiene(54, [5, 7, 99, 3, 4, 54, 2, 12])
-assert.equal(resultado, true,`contiene(54, [5, 7, 99, 3, 4, 54, 2, 12]) debería retornar true pero fue ${resultado}`);
-})
-
-it("Si hacemos contiene(103, [5, 7, 99, 3, 4, 54, 2, 12]) debería retornar false ", function() {
-let resultado = contiene(103, [5, 7, 99, 3, 4, 54, 2, 12])
-assert.equal(resultado, false,`contiene(103, [5, 7, 99, 3, 4, 54, 2, 12]) debería retornar false pero fue ${resultado}`);
-})
